da073f69e9bb8bebc637d94b060ac0ca1cc6a08b
Diogo Moretti <diogo.pearljam@gmail.com>
2013-12-05 15:34:34 -0200

    Add ajax feature

--- src/jquery.nilsleter.js
+++ src/jquery.nilsleter.js
@@ -21,104 +21,104 @@
         Plugin.prototype = {
                 init: function () {
 
-                    // Get values    
-                    var $el      = $( this.element ),
-                        settings = this.settings;
+                        // Get values    
+                        var $el      = $( this.element ),
+                            settings = this.settings;
 
-                    // Labels
-                    label = {
-                        title:   'nilsleter',
-                        nome:    'Nome',
-                        email:   'E-mail',
-                        estado:  'Estado',
-                        nivel:   'Nível',
-                        escolha: 'Escolha'
-                    },
+                        // Labels
+                        label = {
+                            title:   'nilsleter',
+                            nome:    'Nome',
+                            email:   'E-mail',
+                            estado:  'Estado',
+                            nivel:   'Nível',
+                            escolha: 'Escolha'
+                        },
 
-                    // Form
-                    form_open =  '<form class="form-horizontal nl-form-item" role="form">';
-                    form_title = '<h1>'+label.title+'</h1>';
+                        // Form
+                        form_open =  '<form class="form-horizontal nl-form-item" role="form">';
+                        form_title = '<h1>'+label.title+'</h1>';
 
-                    form_name  = ' <div class="form-group">';
-                    form_name += '  <label for="nl-input-nome" class="col-sm-3 control-label">'+label.nome+'</label>';
-                    form_name += '    <div class="col-sm-9">';
-                    form_name += '      <input type="text" class="form-control" id="nl-input-nome" placeholder="'+label.nome+'">';
-                    form_name += '    </div>';
-                    form_name += ' </div>';
+                        form_name  = ' <div class="form-group">';
+                        form_name += '  <label for="nl-input-nome" class="col-sm-3 control-label">'+label.nome+'</label>';
+                        form_name += '    <div class="col-sm-9">';
+                        form_name += '      <input type="text" class="form-control" id="nl-input-nome" value="Name Test" placeholder="'+label.nome+'">';
+                        form_name += '    </div>';
+                        form_name += ' </div>';
 
-                    form_email  = ' <div class="form-group">';
-                    form_email += '  <label for="nl-input-email" class="col-sm-3 control-label">'+label.email+'</label>';
-                    form_email += '    <div class="col-sm-9">';
-                    form_email += '      <input type="email" class="form-control" id="nl-input-email" placeholder="'+label.email+'">';
-                    form_email += '    </div>';
-                    form_email += ' </div>';
+                        form_email  = ' <div class="form-group">';
+                        form_email += '  <label for="nl-input-email" class="col-sm-3 control-label">'+label.email+'</label>';
+                        form_email += '    <div class="col-sm-9">';
+                        form_email += '      <input type="email" class="form-control" id="nl-input-email" value="d@c.com" placeholder="'+label.email+'">';
+                        form_email += '    </div>';
+                        form_email += ' </div>';
 
-                    form_estado  = ' <div class="form-group">';
-                    form_estado += '  <label for="nl-select-estado" class="col-sm-3 control-label">'+label.estado+'</label>';
-                    form_estado += '    <div class="col-sm-9">';
-                    form_estado += '      <select class="form-control" id="nl-select-estado">';
-                    form_estado += '      <option>-- '+label.escolha+' seu '+label.estado+' --</option>';
-                    $.each(settings.fields.estado, function( index, value ) {form_estado += '<option>'+value+'</option>';});
-                    form_estado += '      </select>';
-                    form_estado += '    </div>';
-                    form_estado += ' </div>';
+                        form_estado  = ' <div class="form-group">';
+                        form_estado += '  <label for="nl-select-estado" class="col-sm-3 control-label">'+label.estado+'</label>';
+                        form_estado += '    <div class="col-sm-9">';
+                        form_estado += '      <select class="form-control" id="nl-select-estado">';
+                        form_estado += '      <option>-- '+label.escolha+' seu '+label.estado+' --</option>';
+                        $.each(settings.fields.estado, function( index, value ) {form_estado += '<option>'+value+'</option>';});
+                        form_estado += '      </select>';
+                        form_estado += '    </div>';
+                        form_estado += ' </div>';
 
-                    form_nivel  = ' <div class="form-group">';
-                    form_nivel += '  <label for="nl-select-nivel" class="col-sm-3 control-label">'+label.nivel+'</label>';
-                    form_nivel += '    <div class="col-sm-9">';
-                    form_nivel += '      <select class="form-control" id="nl-select-nivel">';
-                    form_nivel += '      <option>-- '+label.escolha+' seu '+label.nivel+' --</option>';
-                    $.each(settings.fields.nivel, function( index, value ) {form_nivel += '<option>'+value+'</option>';});
-                    form_nivel += '      </select>';
-                    form_nivel += '    </div>';
-                    form_nivel += ' </div>';
+                        form_nivel  = ' <div class="form-group">';
+                        form_nivel += '  <label for="nl-select-nivel" class="col-sm-3 control-label">'+label.nivel+'</label>';
+                        form_nivel += '    <div class="col-sm-9">';
+                        form_nivel += '      <select class="form-control" id="nl-select-nivel">';
+                        form_nivel += '      <option>-- '+label.escolha+' seu '+label.nivel+' --</option>';
+                        $.each(settings.fields.nivel, function( index, value ) {form_nivel += '<option>'+value+'</option>';});
+                        form_nivel += '      </select>';
+                        form_nivel += '    </div>';
+                        form_nivel += ' </div>';
 
-                    form_submit  = '<div class="form-group">';
-                    form_submit += '  <div class="col-sm-5 col-sm-offset-3 loading-box">';
-                    form_submit += '    <span class="loading-item">Loading...</span>';
-                    form_submit += '  </div>';
-                    form_submit += '  <div class="col-sm-4">';
-                    form_submit += '    <button type="submit" class="btn btn-success btn-lg pull-right nl-btn-submit">Enviar</button>';
-                    form_submit += '  </div>';
-                    form_submit += '</div>';
+                        form_submit  = '<div class="form-group">';
+                        form_submit += '  <div class="col-sm-5 col-sm-offset-3 loading-box">';
+                        form_submit += '    <span class="loading-item"></span>';
+                        form_submit += '  </div>';
+                        form_submit += '  <div class="col-sm-4">';
+                        form_submit += '    <button type="submit" class="btn btn-success btn-lg pull-right nl-btn-submit">Enviar</button>';
+                        form_submit += '  </div>';
+                        form_submit += '</div>';
 
-                    form_close = '</form>';
+                        form_close = '</form>';
 
-                    // Form Modal
-                    modal_open  = '<div class="modal fade" id="nilsleter-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-                    modal_open += '  <div class="modal-dialog">';
-                    modal_open += '    <div class="modal-content">';
+                        // Form Modal
+                        modal_open  = '<div class="modal fade" id="nilsleter-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
+                        modal_open += '  <div class="modal-dialog">';
+                        modal_open += '    <div class="modal-content">';
 
-                    modal_header  = '<div class="modal-header">';
-                    modal_header += '  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
-                    modal_header += '  <h4 class="modal-title">'+label.title+'</h4>';
-                    modal_header += '</div>';
+                        modal_header  = '<div class="modal-header">';
+                        modal_header += '  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
+                        modal_header += '  <h4 class="modal-title">'+label.title+'</h4>';
+                        modal_header += '</div>';
 
-                    modal_content_open  = '<div class="modal-body">';
-                    modal_content_close = '</div>';
+                        modal_content_open  = '<div class="modal-body">';
+                        modal_content_close = '</div>';
 
-                    modal_close = '</div></div></div>';
+                        modal_close = '</div></div></div>';
 
-                    // Concat var for form
-                    btnModal = '<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#nilsleter-modal">Quero receber novidades</button>';
-                    formFull  = null;
-                    formInner = form_open + form_name + form_email + form_estado + form_nivel + form_submit + form_close;
+                        // Concat var for form
+                        btnModal = '<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#nilsleter-modal">Quero receber novidades</button>';
+                        formFull  = null;
+                        formInner = form_open + form_name + form_email + form_estado + form_nivel + form_submit + form_close;
 
-                    formItemPage  = form_open + form_title + form_name + form_email + form_estado + form_nivel + form_submit + form_close;
-                    formItemModal = modal_open + modal_header + modal_content_open + formInner + modal_content_close + modal_close;
+                        formItemPage  = form_open + form_title + form_name + form_email + form_estado + form_nivel + form_submit + form_close;
+                        formItemModal = modal_open + modal_header + modal_content_open + formInner + modal_content_close + modal_close;
 
-                    if (settings.modal) {
-                        formFull = formItemModal;
-                    } else {
-                        formFull = formItemPage;
-                        btnModal = null;
-                    }
+                        if (settings.modal) {
+                            formFull = formItemModal;
+                        } else {
+                            formFull = formItemPage;
+                            btnModal = null;
+                        }
 
-                    // Make Form
-                    this.makeForm($el, formFull, btnModal);
+                        // Make Form
+                        this.makeForm($el, formFull, btnModal);
 
-                    // Submit form
-                    $el.find('.nl-form-item').submit( this.submitForm );
+                        // Submit form
+                        $el.find('.nl-form-item').submit( this.submitForm );
                 },
 
                 makeForm: function ( element, form, btnModal ) {
@@ -137,6 +137,10 @@
 
                         formValido = false;
 
+                        $('.loading-item').text('Sending...');
+
+                        $('.nl-btn-submit').attr('disabled','disabled');
+
                         // Validate
                         $('.nl-form-item input').map(function() {
                             if ($(this).val().length === 0) {
@@ -151,10 +155,12 @@
                         // Form validate
                         if (formValido) {
                             dados = JSON.stringify({
-                                nome: nomeValue,
-                                email: emailValue,
+                                token:  $.md5(nomeValue),
+                                secret: $.md5(emailValue),
+                                nome:   nomeValue,
+                                email:  emailValue,
                                 estado: estadoValue,
-                                nivel: nivelValue
+                                nivel:  nivelValue
                             });
                             Plugin.prototype.sendData(dados);
                         }
@@ -162,19 +168,22 @@
 
                 sendData: function ( dados ) {
                         console.log(dados);
-                        //$.ajax ({
-                        //    type: 'post',
-                        //    url: '/sent/' + dados,
-                        //    dataType: 'json',
-                        //    success: function(data) {
-                        //        console.log(data);
-                        //        alert(data.result);
-                        //    },
-                        //    error: function(data) {
-                        //        console.log(data);
-                        //        alert(data.result);
-                        //    }
-                        //});
+                        $.ajax ({
+                            type: 'GET',
+                            url: '/sent/' + dados,
+                            dataType: 'json',
+                            success: function() {
+                                $('.loading-item').addClass('loading-success').text('Success!');
+                            },
+                            error: function() {
+                                $('.loading-item').addClass('loading-error').text('Error! Try again.');
+                            }
+                        });
+
+                        setTimeout(function() {
+                            $('.loading-item').removeClass('loading-error loading-success').text('');
+                            $('.nl-btn-submit').removeAttr('disabled');
+                        }, 3000);
                 }
         };
 
